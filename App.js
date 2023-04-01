49365162404513b835fa92f61870997a52f1de8b
rohith <rohith.goli@anywhere.co>
2023-04-01 14:29:30 +0530

    e3 p4

--- App.js
+++ App.js
@@ -1,34 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// React.createElement => ReactElement - JS Object => HTML element (when rendered)
+
+const heading = React.createElement(
+    "h1",
+    {id: "heading"},
+    "Namaste React"
+);
+
+console.log(heading);
+
+// JSX - HTML-like or XML-like syntax
+// JSX => Babel transpiles it to React.createElement => React Element - JS Object => HTMLElement(render)
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>
+
+console.log(jsxHeading);
+
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
-
-// const heading = React.createElement(
-//     "h1", 
-//     {
-//         id: "heading",
-//         xyz: "abc"
-//     }, 
-//     "Hello World from React"
-// );
-
-// console.log(heading);
-
-
-// root.render(heading);
-
-
-const parentElement = React.createElement("div", {id: "parent1"}, [
-    React.createElement("div", {id: "child1"}, [
-        React.createElement("h1", {}, "Teacher: Akshay Saini"),
-        React.createElement("h1", {}, "Learner: Rohith Goli")
-    ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "Namaste React by Akshay Saini"),
-        React.createElement("h1", {}, "Rohith Goli")
-    ])
-]);
-
-root.render(parentElement);
-
+root.render(jsxHeading);
