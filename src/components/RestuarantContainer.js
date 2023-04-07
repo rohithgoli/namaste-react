8f0a3fba7ef7350216b4880672951dd2101c4050
rohith <rohith.goli@anywhere.co>
2023-04-07 23:47:07 +0530

    react behind the scenes

--- src/components/RestuarantContainer.js
+++ src/components/RestuarantContainer.js
@@ -1,11 +1,27 @@
 import RestuarantCard from "./RestuarantCard";
 import restuarantCardData from "../utils/mockData";
+import {useState} from 'react';
+
 
 const RestuarantContainer = () => {
+
+    // Local State Variable --> Super Powerful Variable
+    const [listOfRestuarants, setListOfRestuarants] = useState(restuarantCardData);
+
+
     return (
         <div className="restuarant-container">
+            <button 
+                type="button" 
+                onClick={() => {
+                    const filteredList = listOfRestuarants.filter(restuarant => restuarant.data.avgRating >= 4)
+                    setListOfRestuarants(filteredList);
+                }}
+            >
+                Get Top Rated Restuarants 
+            </button>
             {
-                restuarantCardData.map(restuarantData => <RestuarantCard key={restuarantData.data.uuid} restuarantData={restuarantData}/>)
+                listOfRestuarants.map(restuarantData => <RestuarantCard key={restuarantData.data.uuid} restuarantData={restuarantData}/>)
             }
         </div>
     )
