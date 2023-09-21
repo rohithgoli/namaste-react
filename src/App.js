db6af9c4b6f3e0f7c6f7265a70bc6d0518d89631
Rohith Goli <rohithgoli396@gmail.com>
2023-09-21 15:48:05 +0530

    feat: basic routing with react-router-dom v6 using createBrowserRouter, RouterProvider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
 
 const App = () => {
     return(
@@ -15,27 +19,22 @@ const App = () => {
     )
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const appRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />
+    },
+    {
+        path: '/about',
+        element: <About />
+    },
+    {
+        path: '/contact',
+        element: <Contact />
+    }
+])
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App/>);
+root.render(<RouterProvider router={appRouter} />);
